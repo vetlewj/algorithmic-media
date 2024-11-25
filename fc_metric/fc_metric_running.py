06d56afaff473563e4fb04bbf5c8be0bc3a339e3
julianstrietzel <julian.strietzel@tum.de>
2024-11-24 16:34:19 -0800

    fc metric running for repos twdm included

--- fc_metric/fc_metric_running.py
+++ fc_metric/fc_metric_running.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from tqdm import tqdm
 
 from fc_metric import calculate_fc_using_gpt
 
+tqdm.pandas()
+
 filename = "filtered_data_with_abstracts_repo.csv"
 df = pd.read_csv("../data/" + filename)
-df["fc_metric"] = df.apply(
+df["fc_metric"] = df.progress_apply(
     lambda x: calculate_fc_using_gpt(
         x["sem_scholar_abstract"], x["title"], x["sem_scholar_title"], False
     ),
